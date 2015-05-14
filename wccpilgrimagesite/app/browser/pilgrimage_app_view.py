0d7d32c4e098494e686f273bab1378c5e44625a1
glenn <glenn@glenn-Lenovo-G480>
2015-05-15 00:08:58 +0800

    added new functions

--- wccpilgrimagesite/app/browser/pilgrimage_app_view.py
+++ wccpilgrimagesite/app/browser/pilgrimage_app_view.py
@@ -1,6 +1,7 @@
 from five import grok
 from plone.directives import dexterity, form
 from wccpilgrimagesite.app.content.pilgrimage_app import IPilgrimageApp
+from Products.CMFCore.utils import getToolByName
 
 grok.templatedir('templates')
 
@@ -9,4 +10,29 @@ class Index(dexterity.DisplayForm):
     grok.require('zope2.View')
     grok.template('pilgrimage_app_view')
     grok.name('view')
+    
+    @property
+    def catalog(self):
+        return getToolByName(self.context, 'portal_catalog')
+    
+    def pilgrimage_steps(self):
+        results = []
+        path = '/'.join(self.context.getPhysicalPath())
+        brains = self.catalog.unrestrictedSearchResults(path={'query':path, 'depth':1}, portal_type='wccpilgrimagesite.app.pilgrimagesteps')
+        for brain in brains:
+            obj = brain._unrestrictedGetObject()
+            parent = obj.aq_parent
+            data = {'position':parent.getObjectPosition(brain.id) + 1}
+            data['title'] = brain.Title
+            data['url'] = brain.getPath()
+            data['obj'] = obj
+            data['image'] = obj.image
+            
+            results.append(data)
+        if results:
+            results.sort(key=lambda result:result['position'])
+        return results
+            
+        
+    
 
