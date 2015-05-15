d6dbdd47c72c9c4d6e128c146654979459a38fd9
sescatalan <sandrea@afterfivetech.com>
2015-05-15 11:26:18 +0800

    added resource on pilgrimage resource browser view and made video, sound and document fields required false

--- wccpilgrimagesite/app/browser/pilgrimage_steps_view.py
+++ wccpilgrimagesite/app/browser/pilgrimage_steps_view.py
@@ -19,5 +19,61 @@ class Index(dexterity.DisplayForm):
         brains = catalog.searchResults(path={'query':path, 'depth':1}, portal_type='wccpilgrimagesite.app.usercomment')[:3]
 	return brains
 
+    def resources_result(self):
+        context = self.context
+        catalog = getToolByName(context, 'portal_catalog')
+        path = '/'.join(context.getPhysicalPath())
+        result = []
+        brains = catalog.searchResults(path={'query':path, 'depth':3}, portal_type='wccpilgrimagesite.app.resourceupload',sort_on='Date',
+                sort_order='reverse',)
+        for brain in brains:
+            obj = brain._unrestrictedGetObject()
+            if obj.video:
+                data= {'title': obj.title,
+                        'name': obj.name,
+                        'email':obj.email,
+                        'church': obj.church,
+                        'message': obj.message,
+                        'resource':obj.video,
+                        'type': "video",
+                        'path':brain.getPath(),
+                }
+                result.append(data)
+                break;
+
+        for brain in brains:
+            obj = brain._unrestrictedGetObject()
+            if obj.sound:
+                data= {'title': obj.title,
+                        'name': obj.name,
+                        'email':obj.email,
+                        'church': obj.church,
+                        'message': obj.message,
+                        'resource':obj.sound,
+                        'type': "sound",
+                        'path':brain.getPath(),
+                }
+                result.append(data)
+                break;
+
+        for brain in brains:
+            obj = brain._unrestrictedGetObject()
+            if obj.document:
+                data= {'title': obj.title,
+                        'name': obj.name,
+                        'email':obj.email,
+                        'church': obj.church,
+                        'message': obj.message,
+                        'resource':obj.document.filename,
+                        'type': "document",
+                        'path':brain.getPath(),
+                }
+                result.append(data)
+                break;
+
+
+        return result
+
+
     def datetime_result(self, value=None):
         return value.strftime("%Y-%m-%d %H:%M")
