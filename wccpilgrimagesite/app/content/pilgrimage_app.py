5e16e489c0d6e0789ddd9725bfbda3f7157bad08
sescatalan <sandrea@afterfivetech.com>
2015-05-22 14:51:20 +0800

    modified id format

--- wccpilgrimagesite/app/content/pilgrimage_app.py
+++ wccpilgrimagesite/app/content/pilgrimage_app.py
@@ -29,6 +29,8 @@ from plone.portlets.interfaces import IPortletManager, IPortletAssignmentMapping
 from wccpilgrimagesite.app.portlet import burgermenuportlet
 
 from wccpilgrimagesite.app import MessageFactory as _
+from Products.CMFCore.utils import getToolByName
+from plone.i18n.normalizer import idnormalizer
 
 
 # Interface class; used to define content-type schema.
@@ -60,6 +62,31 @@ def _createObj(context, event):
     chooser = INameChooser(manager)
     assignment.path = '/'.join(context.getPhysicalPath())
     manager[chooser.chooseName(None, assignment)] = assignment
+
+    id = context.getId()
+    object_Ids = []
+    catalog = getToolByName(context, 'portal_catalog')
+    brains = catalog.unrestrictedSearchResults(object_provides = IPilgrimageApp.__identifier__)
+    for brain in brains:
+        object_Ids.append(brain.id)
+    
+    last_name = str(idnormalizer.normalize(context.title))
+    temp_new_id = last_name
+    new_id = temp_new_id.replace("-","")
+    test = ''
+    if new_id in object_Ids:
+        test = filter(lambda name: new_id in name, object_Ids)
+        if '-' not in (max(test)):
+            new_id = new_id + '-1'
+        if '-' in (max(test)):
+            new_id = new_id +'-' +str(int(max(test).split('-')[-1])+1) 
+
+    parent.manage_renameObject(id, new_id )
+    new_title = last_name
+    context.setTitle(context.title)
+
+
+    context.reindexObject()
     return
 
 
