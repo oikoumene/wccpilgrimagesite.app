fe65a50abea86a88cf14b0f3f2af75e3efc40683
glenn <glenn@glenn-Lenovo-G480>
2015-06-03 16:34:04 +0800

    disabled creation of resource upload

--- wccpilgrimagesite/app/browser/api_add_resourceupload.py
+++ wccpilgrimagesite/app/browser/api_add_resourceupload.py
@@ -30,34 +30,40 @@ class api_add_resourceupload(grok.View):
         context = self.context
         name = ''
         church = ''
+        description = ''
+        email = ''
         parent_path = '/'.join(context.getPhysicalPath())
         if request.form:
             form = request.form
             if 'name' in form:
-                item = createContentInContainer(context, 'wccpilgrimagesite.app.resourceupload', checkConstraints=False, title=u"Resource Upload")
-                setattr(item, 'title', "Resource Upload")
-                item.name = form['name']
+                #item = createContentInContainer(context, 'wccpilgrimagesite.app.resourceupload', checkConstraints=False, title=u"Resource Upload")
+                #setattr(item, 'title', "Resource Upload")
+                #item.name = form['name']
                 name = form['name']
                 
                 
                 if 'email' in form:
-                    item.email = form['email']
+                    #item.email = form['email']
+                    email = form['email']
                     
                 if 'church' in form:
-                    item.church = form['church']
+                    #item.church = form['church']
                     church = form['church']
                     
                 if 'message' in form:
-                    item.message = form['message']
+                    #item.message = form['message']
+                    description = form['message']
                     
                 if 'video' in form:
-                    item.video = form['video']
+                    #item.video = form['video']
                     if form['video']:
                         video_item = createContentInContainer(context, 'wccpilgrimagesite.app.video', checkConstraints=False, title=form['name']+' Video')
                         
                         video_item.title = name
                         video_item.url_youtube = form['video']
                         video_item.church = church
+                        video_item.uploader = name
+                        video_item.email = email
                         
                         video_id = self.generate_id(parent_path, str(idnormalizer.normalize(form['name']+' Video')))
                         if video_id:
@@ -65,13 +71,15 @@ class api_add_resourceupload(grok.View):
                         video_item.reindexObject()
                     
                 if 'sound' in form:
-                    item.sound = form['sound']
+                    #item.sound = form['sound']
                     if form['sound']:
                         sound_item = createContentInContainer(context, 'wccpilgrimagesite.app.sound', checkConstraints=False, title=form['name']+' Sound')
                         
                         sound_item.title = name
                         sound_item.soundcloud_id = form['sound']
                         sound_item.church = church
+                        sound_item.uploader = name
+                        sound_item.email = email
                         
                         sound_id = self.generate_id(parent_path, str(idnormalizer.normalize(form['name']+' Sound')))
                         if sound_id:
@@ -79,13 +87,15 @@ class api_add_resourceupload(grok.View):
                         sound_item.reindexObject()
                     
                 if 'docName' in form and 'docData' in form:
-                    item.document = namedfile.NamedBlobFile(
-                        base64.b64decode(form['docData'].split(';base64,')[1]),
-                        filename = form['docName'].decode('utf-8', 'ignore')
-                    )
+                    #item.document = namedfile.NamedBlobFile(
+                    #    base64.b64decode(form['docData'].split(';base64,')[1]),
+                    #    filename = form['docName'].decode('utf-8', 'ignore')
+                    #)
                     doc_item = createContentInContainer(context, 'wccpilgrimagesite.app.staticdocument', checkConstraints=False, title=form['name']+' Document')
                     doc_item.title = name
                     doc_item.church = church
+                    doc_item.uploader = name
+                    doc_item.email = email
                     doc_item.file = namedfile.NamedBlobFile(
                         base64.b64decode(form['docData'].split(';base64,')[1]),
                         filename = form['docName'].decode('utf-8', 'ignore')
@@ -95,10 +105,10 @@ class api_add_resourceupload(grok.View):
                         context.manage_renameObject(doc_item.id, doc_id)
                     doc_item.reindexObject()
                     
-                id = self.generate_id(parent_path, 'resource-upload')
-                if id:
-                    context.manage_renameObject(item.id, id)
-                item.reindexObject()
+                #id = self.generate_id(parent_path, 'resource-upload')
+                #if id:
+                #    context.manage_renameObject(item.id, id)
+                #item.reindexObject()
                 
                 
                 
